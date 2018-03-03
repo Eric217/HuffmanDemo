fc95ed0b7b88337a0b0c2f6985a91405860d740e
Eric <erisl@iCloud.com>
2018-03-03 21:59:53 +0800

    decode and save

---
+++ Huffman/Huffman/MainView/NSViewController+tools.h
@@ -0,0 +1,13 @@
+//
+//  NSViewController+tools.h
+//  Huffman
+//
+//  Created by Eric on 03/03/2018.
+//  Copyright © 2018 Eric. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSViewController (tools)
+
+@end
---
+++ Huffman/Huffman/MainView/NSViewController+tools.m
@@ -0,0 +1,13 @@
+//
+//  NSViewController+tools.m
+//  Huffman
+//
+//  Created by Eric on 03/03/2018.
+//  Copyright © 2018 Eric. All rights reserved.
+//
+
+#import "NSViewController+tools.h"
+
+@implementation NSViewController (tools)
+
+@end
